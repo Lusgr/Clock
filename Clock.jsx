0e6d31856759ab0b960efbe2c623115d5977c54f
Lusgr <136743895+Lusgr@users.noreply.github.com>
2023-07-17 14:37:09 +0400

    Add files via upload

--- Clock.jsx
+++ Clock.jsx
@@ -1,38 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
-
-/*const Clock = ()=>{
-    const [time, setTime] = useState(new Date())
-
-    useEffect(()=>{
-        const interval = setInterval(()=>{
-            setTime(new Date())
-        }, 1000)
-
-        return()=>{ clearInterval(interval)}
-    },[])
-
-    const formateTime = (date) =>{
-        const hours = date.getHours()
-        const minuts = date.getMinutes()
-        const seconds = date.getSeconds()
-
-        return `${hours} : ${minuts} : ${seconds}`
-    }
-    
-    
-    
-
-    return(
-        <>
-            <div>
-                <p>Clock</p>
-                <h2>{formateTime(time)}</h2>
-            </div>
-        </>
-    )
-}*/
-
 const Clock = ()=>{
 
     const [time, setTime] = useState(new Date())
